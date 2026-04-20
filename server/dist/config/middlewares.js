4284373f29b1c5a7a0c98ccb0d4ef8b1871b4189
ZCRT581-IN-NB2 <zcrt581-in-nb2@ZCRT581-IN-NB2.local>
2026-04-20 12:14:43 +0530

    added vercel

--- server/dist/config/middlewares.js
+++ server/dist/config/middlewares.js
@@ -1,10 +1,20 @@
 "use strict";
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const config = [
     'strapi::logger',
     'strapi::errors',
     'strapi::security',
-    'strapi::cors',
+    {
+        name: 'strapi::cors',
+        config: {
+            origin: [
+                'http://localhost:3000',
+                'https://freecodecampstrapi.vercel.app',
+                (_a = process.env.ALLOWED_ORIGINS) !== null && _a !== void 0 ? _a : '',
+            ].filter(Boolean),
+        },
+    },
     'strapi::poweredBy',
     'strapi::query',
     'strapi::body',
